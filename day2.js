cae4250d2b79154d02c89a85ebf13947e051f084
rahulrajput83 <rahulrajput21112@gmail.com>
2022-10-08 14:35:21 +0530

    Score Greater than 85

--- day2.js
+++ day2.js
@@ -45,4 +45,65 @@ function createArray() {
     return newArray;
 }
 
-console.log(createArray());
+/* console.log(createArray()); */
+
+
+
+/*  2.	Write a function which filter users who has  scoresGreaterThan85,  
+	Write a function which addUser  to the user array only if the user does not exist.   
+	Write a function which addUserSkill which can add skill to a user only if the user exist.   
+	Write a function which editUser if the user exist in the users array.  
+	```js */
+
+	const users = [
+	{
+		name:'Brook', 
+		scores:75,
+		skills:['HTM', 'CSS', 'JS'],
+		age:16
+	},
+	{
+		name:'Alex', 
+		scores:80,
+		skills:['HTM', 'CSS', 'JS'],
+		age:18
+	}, 
+	{
+		name:'David', 
+		scores:75,
+		skills:['HTM', 'CSS'],
+		age:22
+	}, 
+	{
+		name:'John', 
+		scores:85,
+		skills:['HTM'],
+		age:25
+	},
+	{
+		name:'Sara',
+		scores:95,
+		skills:['HTM', 'CSS', 'JS'],
+		age: 26
+	},
+	{
+		name:'Martha', 
+		scores:80,
+		skills:['HTM', 'CSS', 'JS'],
+		age:18
+	},
+	{
+		name:'Thomas',
+		scores:90,
+		skills:['HTM', 'CSS', 'JS'],
+		age:20
+	}
+	]
+
+/* Write a function which filter users who has  scoresGreaterThan85 */
+
+function GreateScore(arr) {
+    return arr.filter((item) => item.scores > 85)
+}
+
+console.log(GreateScore(users))
